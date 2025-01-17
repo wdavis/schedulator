3745d8077a1fd704b33ea0935a5fbdb2bae804eb
Shift <shift@laravelshift.com>
2025-01-17 16:10:12 +0000

    Re-register exception handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Contracts\HttpStatusContract;
 use App\Providers\AppServiceProvider;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withProviders()
@@ -25,5 +31,43 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->reportable(function (Throwable $e) {
+            //
+        });
+
+        $exceptions->renderable(function (Throwable $e, Request $request) {
+            if ($request->is('v1/*')) {
+
+                $message = $e->getMessage();
+
+                $status = 500;
+                if ($e instanceof HttpStatusContract) {
+                    $status = $e->getHttpStatusCode();
+                }
+
+                if ($e instanceof ModelNotFoundException || $e instanceof NotFoundHttpException) {
+                    $status = 404;
+                    if (preg_match('/No query results for model \[App\\\Models\\\(.*?)]\./', $e->getMessage(), $matches)) {
+                        $modelName = $matches[1]; // This will contain the model name e.g. 'Booking'
+                        $message = "$modelName not found.";
+                    }
+                }
+
+                if ($e instanceof QueryException) {
+                    $status = 400;
+                    $message = 'Bad request.';
+                }
+
+                if ($e instanceof ThrottleRequestsException) {
+                    $status = 429;
+                    $message = 'Too many requests.';
+                }
+
+                return response()->json([
+                    'message' => $message,
+                ], $status);
+            }
+
+            return parent::render($request, $e);
+        });
     })->create();
